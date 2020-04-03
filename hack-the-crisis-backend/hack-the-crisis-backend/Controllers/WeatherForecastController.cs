deeea0230f7dc5884ab2841bf759245ae3a29136
KrisztinaFoldi <krisztifoldi11@gmail.com>
2020-04-03 16:41:07 +0200

    initial repo

---
+++ hack-the-crisis-backend/hack-the-crisis-backend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hack_the_crisis_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        
+    }
+}
